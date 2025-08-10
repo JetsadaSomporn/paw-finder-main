502850c53542c7ed9359a3e5e21accf7ad510376
Jetsada Somporn <jetsadasomporn2003@gmail.com>
2025-08-10 16:08:35 +0700

    clean code

--- src/shared/components/ImageUpload.tsx
+++ src/shared/components/ImageUpload.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { X, Image as ImageIcon } from 'lucide-react';
 
